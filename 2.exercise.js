f9c5bae41faf05fecbb7a9f965d3a0549f932e8c
lixin-940207 <alix.xinli@gmail.com>
2021-05-29 15:45:35 +0200

    some exercises

---
+++ 2.exercise.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+
+// fs.readFile('./resource/content.txt', (err, data) => {
+//     if(err){
+//         throw err;
+//     }
+//     console.log(data.toString())
+// })
+
+let p = new Promise(((resolve, reject) => {
+    fs.readFile('./resource/content.txt', (err, data) => {
+        if(err){
+            reject(err)
+        }
+        resolve(data)
+    })
+}))
+
+p.then(value => {
+    console.log(value.toString())
+    },
+        reason => {
+    console.log(reason)
+})
